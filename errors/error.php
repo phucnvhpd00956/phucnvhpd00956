10af088e5a76e677efe2ab61f7a0af23b2019e0b
phucnvhpd00956 <phucnvhcntt@gmail.com>
2015-08-11 20:36:41 +0700

    Revert "phucnvhpd00956"
    This reverts commit 66261778f43bd37621654f24aa102efc26670c4f.

--- errors/error.php
+++
@@ -1,6 +0,0 @@
-<?php include 'view/header.php'; ?>
-<div id="content">
-    <h2>Error</h2>
-    <p><?php echo $error_message; ?></p>
-</div>
-<?php include 'view/footer.php'; ?>
--- view/header_admin.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE HTML>
-<head>
-<title>PEPSI</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" media="screen" href="css/style1.css">
-<link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
-</head>
